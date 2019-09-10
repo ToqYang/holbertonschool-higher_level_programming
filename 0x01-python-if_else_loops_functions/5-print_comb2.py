3ec2e120d91c2da3a28260e59b27c5cfa6114ecb
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-10 21:15:58 +0000

    [Fix] Format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("0", end="")
     if i <= 98:
-        print("{:d}, ".format(i), end="")
+        print("{:02d}, ".format(i), end="")
     else:
         print("{:d}\n".format(i))
