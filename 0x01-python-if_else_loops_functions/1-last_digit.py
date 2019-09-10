07ca20d310648b5e8012a860cb1c804d382902f7
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-10 19:46:49 +0000

    [Fix] Order of if

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,9 @@ elif number > 0:
     last_Number = number % 10
 print("Last digit of {:d}".format(number), end=' ')
 print("is {:d}".format(last_Number), end=' ')
-if last_Number == 0:
-    print("and is 0")
-elif last_Number > 5:
+if last_Number > 5:
     print("and is greater than 5")
+elif last_Number == 0:
+    print("and is 0")
 else:
     print("and is less than 6 and not 0")
