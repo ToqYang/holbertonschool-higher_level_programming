e065e62a74dea5d7113007e03a861235c5087958
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-12 22:11:15 +0000

    [Fix] Output message

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
-        print("{:d} arguments.".format(len(sys.argv) - 1))
+    if len(sys.argv) != 2:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+        for i, j in enumerate(sys.argv):
+            if i != 0 and j != 0:
+                print("{}: {}".format(i, j))
     else:
+        print("{:d} argument.".format(len(sys.argv) - 1))
         for i, j in enumerate(sys.argv):
             if i != 0 and j != 0:
                 print("{}: {}".format(i, j))
