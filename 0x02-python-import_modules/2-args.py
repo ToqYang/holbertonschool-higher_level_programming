cf8e98ac2d5c1538b098cfabb6705d83fbf618d9
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-12 22:34:24 +0000

    [Fix] Pep8

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
         print("{:d} argument:".format(len(sys.argv) - 1))
         for i, j in enumerate(sys.argv):
             if i != 0 and j != 0:
-               print("{}: {}".format(i, j))
+                print("{}: {}".format(i, j))
     if len(sys.argv) > 2:
         print("{:d} arguments:".format(len(sys.argv) - 1))
         for i, j in enumerate(sys.argv):
             if i != 0 and j != 0:
-               print("{}: {}".format(i, j))
+                print("{}: {}".format(i, j))
