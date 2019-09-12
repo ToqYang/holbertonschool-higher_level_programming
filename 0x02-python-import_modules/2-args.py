9495959a3d744e1c3238980c6970fc2cf364752a
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-12 22:32:39 +0000

    [Fix] Format Output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    if len(sys.argv) != 2 and len(sys.argv) != 1:
+    if len(sys.argv) == 1:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
+    if len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+        for i, j in enumerate(sys.argv):
+            if i != 0 and j != 0:
+               print("{}: {}".format(i, j))
+    if len(sys.argv) > 2:
         print("{:d} arguments:".format(len(sys.argv) - 1))
         for i, j in enumerate(sys.argv):
             if i != 0 and j != 0:
-                print("{}: {}".format(i, j))
-    else:
-        print("{:d} argument.".format(len(sys.argv) - 1))
-        for i, j in enumerate(sys.argv):
-            if i != 0 and j != 0:
-                print("{}: {}".format(i, j))
+               print("{}: {}".format(i, j))
