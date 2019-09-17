5428ae60a72f80ab4ff456db3de41d700175981c
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-17 20:42:15 +0000

    [Fix] Change len and parts of the code

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     copy_list = list.copy(my_list)
-    lenlist = len(copy_list)
     if my_list is None:
         return
-    if idx < 0 or idx > lenlist:
+    if idx < 0 or idx > len(copy_list):
         return copy_list
-    else:
-        copy_list[idx] = element
-        return copy_list
+
+    copy_list[idx] = element
+    return copy_list
