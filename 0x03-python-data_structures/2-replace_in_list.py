d091479b2097dbcf9922a5f6a4b585b4faa46dc7
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-17 20:11:34 +0000

    [Fix] Introduce loop

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    lenlist = len(my_list)
-    if idx < 0 or idx > lenlist:
+    if idx < 0 or idx > len(my_list):
         return my_list[idx]
-    my_list[idx] = element
+    for x in range(len(my_list)):
+        if idx == x:
+            my_list[idx] = element
     return my_list
