9cbc026a40df3af32d4a6ab9a621e486887a0fe7
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-17 20:06:23 +0000

    [Fix] Length strin

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    lenlist = len(my_list)
-    if idx < 0 and idx > lenlist:
+    if idx < 0 and idx > len(my_list):
         return None
     for x in range(len(my_list)):
         if idx == counter:
