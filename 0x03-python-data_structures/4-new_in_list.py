d0f8b8fd21670e3906c8c3cd0609a50409f3439c
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-17 20:46:33 +0000

    [Fix] Change the manner of copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    copy_list = list.copy(my_list)
+    copy_list = my_list.copy()
     if my_list is None:
         return
     if idx < 0 or idx > len(copy_list):
