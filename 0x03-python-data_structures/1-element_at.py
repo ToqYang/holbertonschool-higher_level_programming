c495e8a3bc587a8f80bc7b1a2f20ed8e716c37aa
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-17 20:04:34 +0000

    [Fix] Operator

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     lenlist = len(my_list)
-    if idx < 0 or idx > lenlist:
+    if idx < 0 and idx > lenlist:
         return None
     for x in range(len(my_list)):
         if idx == counter:
