ecb962208a0fd2829c9eef96090dcb9f7da1245c
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-17 22:05:23 +0000

    [Add-File] Remove characters

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string is None:
+        return
+    counter = 0;
+    new_string = []
+    for x in my_string:
+        new_string.insert(counter, x)
+        if x == 'c':
+            new_string.remove('c')
+        if x == 'C':
+            new_string.remove('C')
+        counter += 1;
+    return new_string
