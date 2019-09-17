ecd93fe95a3956493f664b9a772c73e572b9e186
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-17 20:08:32 +0000

    [Fix] Variable counter

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,5 +3,5 @@ def element_at(my_list, idx):
     if idx < 0 and idx > len(my_list):
         return None
     for x in range(len(my_list)):
-        if idx == counter:
-            return my_list[idx]
+        if idx == x:
+            return my_list[x]
