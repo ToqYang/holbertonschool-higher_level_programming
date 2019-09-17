3c649f6a14fb4f3cf73a0e145583b6e55e49beed
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-17 20:37:48 +0000

    [Fix] Condition empty

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,8 @@
 def new_in_list(my_list, idx, element):
     copy_list = list.copy(my_list)
     lenlist = len(copy_list)
+    if my_list is None:
+        return
     if idx < 0 or idx > lenlist:
         return copy_list
     else:
