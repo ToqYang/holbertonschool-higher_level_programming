68b883ad5679e261294900defe7ab9c1d5e64a6a
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-17 18:00:43 +0000

    [Add-File] Copy list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copy_list = list.copy(my_list)
+    lenlist = len(copy_list)
+    if idx < 0 or idx > lenlist:
+        return copy_list
+    else:
+        copy_list[idx] = element
+        return copy_list
