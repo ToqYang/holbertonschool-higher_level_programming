951dd1fb8787f8e156c354b66b5466d2e1af810a
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-18 00:29:21 +0000

    [Add-File] Max number

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    num_Max = 0
+    for x in my_list:
+        if num_Max < x:
+            num_Max = x
+    return num_Max
