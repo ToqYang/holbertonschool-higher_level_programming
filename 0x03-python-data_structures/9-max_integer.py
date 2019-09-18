a0c9b4b1dc98651a8b960ce25854d32204f2530d
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-18 22:30:37 +0000

    [Fix] Negatives and None

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
-        return None
-    num_Max = 0
+    if len(my_list) is 0:
+        return(None)
+    num_Max = -1
     for x in my_list:
         if num_Max <= x:
             num_Max = x
