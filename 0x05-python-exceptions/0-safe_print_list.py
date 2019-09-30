d47df98d110b661bd7b027b08614f3c062e6b73e
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-30 19:00:50 +0000

    [Fix] Make a variable for count the data

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for a in range(0, x):
+    data = 0
+    for a in range(x):
         try:
             print("{}".format(my_list[a]), end="")
-            a += 1
+            data += 1
         except:
             pass
     print()
-    return a
+    return data
