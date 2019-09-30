bc1823019506625da0c3b5bdb082f8b0859b1341
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-30 18:58:00 +0000

    [Fix] Change place the loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for a in range(0, x):
+    for a in range(0, x):
+        try:
             print("{}".format(my_list[a]), end="")
             a += 1
-    except:
-        pass
+        except:
+            pass
     print()
     return a
