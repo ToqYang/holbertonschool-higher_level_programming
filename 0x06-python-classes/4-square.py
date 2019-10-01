ec6fea0197d946ba3ca7cd89dbf5e20f00ea95ac
Santiago Yanguas <santitoya-2001@outlook.com>
2019-10-01 23:27:52 +0000

    [Feature] Get and set

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+class Square:
+    """ Defining a Square class """
+    def __init__(self, size=0):
+        """ Constructor square class """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Getter property """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Setter property """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
