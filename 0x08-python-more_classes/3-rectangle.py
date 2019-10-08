1065bdf55a290155bd228695e059be9333230b69
Santiago Yanguas <santitoya-2001@outlook.com>
2019-10-08 00:31:35 +0000

    [Fix] Add elif

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -57,7 +57,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        else:
+        elif:
             self.__height = value
 
     def area(self):
