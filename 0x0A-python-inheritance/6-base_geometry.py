6282a2e3ae192fdb64af8ea1c5d25a9ad5903c96
Santiago Yanguas <santitoya-2001@outlook.com>
2019-10-15 21:43:06 +0000

    [Feature] Raise of the area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Make Base Geometry class """
+
+
+class BaseGeometry:
+    """ Empty class """
+    def area(self):
+        """ Function until now print the exception
+            in case of that the functiont doesn't work"""
+        raise Exception("area() is not implemented")
