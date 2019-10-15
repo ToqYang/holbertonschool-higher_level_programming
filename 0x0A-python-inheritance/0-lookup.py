bf54779e3430000ddc3c1ad07b561d66115271f7
Santiago Yanguas <santitoya-2001@outlook.com>
2019-10-15 19:58:33 +0000

    [Fix] Pep8

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Print all the attributes and methods of an object """
+
+
 def lookup(obj):
     """ Return a list with all attributes  and method
         of the object passed how argument """
-    return  dir(obj)
+    return dir(obj)
