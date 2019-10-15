22fd1b51ccc5a14e5c81fcaae53e69271d3a54f5
Santiago Yanguas <santitoya-2001@outlook.com>
2019-10-15 20:43:06 +0000

    [Fix] Pep8

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-class MyList(list):
+""" Inherits from list to the class MyList """
 
+
+class MyList(list):
+    """ Inherits from list for can be return how list """
     def print_sorted(self):
+        """ Print the list sorted of manner ascending"""
         print(sorted(self))
