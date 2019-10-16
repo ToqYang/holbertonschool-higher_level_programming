874f3455872c4df8fd3be88bae26e86459f16d29
Santiago Yanguas <santitoya-2001@outlook.com>
2019-10-16 22:08:22 +0000

    [Feature] Write in a File

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Write in a file determined  """
+
+
+def write_file(filename="", text=""):
+    """ Write in a file with the arguments received
+        String = filename
+        String = text
+    """
+    with open(filename, mode="w", encoding="utf-8") as fil:
+        width_char = fil.write(text)
+    return width_char
