d82be60f5f15a0914e28c17a34865be50a371e1b
Santiago Yanguas <santitoya-2001@outlook.com>
2019-10-16 20:38:56 +0000

    [Fix] Close not is neccesary and i put the format

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,4 @@ def read_file(filename=""):
     """ Received the filename for it can does a syscall for read the file """
     with open(filename, mode="r", encoding="utf-8") as fil:
         rd_Txt = fil.read()
-        print(rd_Txt)
-    fil.close()
+        print(rd_Txt, end="")
