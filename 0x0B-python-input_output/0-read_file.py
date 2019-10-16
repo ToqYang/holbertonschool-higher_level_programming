365ab71cbdf2ba408caaea16bebb500fb1ae1b70
Santiago Yanguas <santitoya-2001@outlook.com>
2019-10-16 20:25:02 +0000

    [Add-File] Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Read a file in UTF8  """
+
+
+def read_file(filename=""):
+    """ Received the filename for it can does a syscall for read the file """
+    with open(filename, mode="r", encoding="utf-8") as fil:
+        rd_Txt = fil.read()
+        print(rd_Txt)
