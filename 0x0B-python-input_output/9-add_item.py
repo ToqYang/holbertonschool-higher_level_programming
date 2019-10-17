d11d0711f1907ccabd7f6e0d141cbf48be3a4959
Santiago Yanguas <santitoya-2001@outlook.com>
2019-10-17 01:59:52 +0000

    [Feature] Add-Item

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Load add, save json file """
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+if __name__ == "__main__":
+    import sys
+    """ Take the argv and append the last value at the JSON file """
+
+    try:
+        filename = "add_item.json"
+        my_list = load_from_json_file(filename)
+    except:
+        filename = "add_item.json"
+        my_list = []
+        save_to_json_file(my_list, filename)
+
+    le = len(sys.argv)
+    counter = 1
+
+    while(counter < le):
+        my_list.append(sys.argv[counter])
+        counter += 1
+
+    save_to_json_file(my_list, filename)
