1abc5fc26e74e4be2863e6035187358ea892ce8a
Santiago Yanguas <santitoya-2001@outlook.com>
2019-11-26 15:03:24 +0000

    [Fix] Set password to version 5.7.8

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,9 @@
--- Create user wit all privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Make user
+-- Set the password because is mysql 5.7
+-- Give all the privileges for example how root
+-- Update privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_2_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
 
