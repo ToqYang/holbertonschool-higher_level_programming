1f13c9ca858ca4a729e8c3f36dee2d075ea2e262
Santiago Yanguas <santitoya-2001@outlook.com>
2019-11-26 17:21:49 +0000

    [Fix] Put correct passwd

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 -- Set the password because is mysql 5.7
 -- Give all the privileges for example how root
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_2_pwd';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
 
