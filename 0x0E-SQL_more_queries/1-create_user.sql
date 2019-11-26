8857f0f89a25c8b24555cecda3a2bc3173ee15cb
Santiago Yanguas <santitoya-2001@outlook.com>
2019-11-26 17:20:33 +0000

    [Fix] Add spaces

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,6 @@
 -- Make user
 -- Set the password because is mysql 5.7
 -- Give all the privileges for example how root
--- Update privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_2_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
