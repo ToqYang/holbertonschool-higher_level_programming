6b834743be0094d70285a17c3ffd3353bb47a002
Santiago Yanguas <santitoya-2001@outlook.com>
2019-11-26 03:39:35 +0000

    [Fix] Semicolon of the limit

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
--- 
+-- Show the top 3 cities more hot
 SELECT city, AVG(value) avg_temp
 FROM temperatures
 WHERE month = 7 OR month = 8
 GROUP BY city DESC
-ORDER BY avg_temp DESC;
-LIMIT 3
+ORDER BY avg_temp DESC
+LIMIT 3;
