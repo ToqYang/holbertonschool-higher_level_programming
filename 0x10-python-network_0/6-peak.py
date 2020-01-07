427eb55faf41f47ea6fcb83562cf53859f15791b
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-07 21:35:46 +0000

    [Fix] Same

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,4 +6,8 @@ def find_peak(list_of_integers):
     """ Find the number highest of a array """
     if not list_of_integers:
         return None
-    return max(list_of_integers)
+    num = 0
+    for elem in list_of_integers:
+        if elem > num:
+            num = elem
+    return num
