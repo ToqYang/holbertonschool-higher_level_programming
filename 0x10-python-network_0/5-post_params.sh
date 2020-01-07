b39f2dd126944f189630890553dad2ba9c0887ea
Santiago <santitoya-2001@outlook.com>
2020-01-07 02:27:02 +0000

    [Fix] Change the  to the

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cURL POST parameters 
-curl -s $2 -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD";
+curl -s $1 -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD";
