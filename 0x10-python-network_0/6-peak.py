a7cd2670277485b2f29f71cefb34d2cfcb4a4a62
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-07 21:23:17 +0000

    Improve performance with function prebuild

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,9 +6,4 @@ def find_peak(list_of_integers):
     """ Find the number highest of a array """
     if not list_of_integers:
         return None
-
-    num = 0
-    for elem in list_of_integers:
-        if elem > num:
-            num = elem
-    return num
+    return max(list_of_integers)
