fdd8610690aa125f654369a2e797244ab1479ed0
Santiago TY <santitoya-2001@outlook.com>
2020-01-13 18:19:32 -0500

    [Fix] Other manner

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ Response header value """
+# I can use headers.get, getheader, getheaders
 import urllib.request
 import sys
 
 
-with urllib.request.urlopen(sys.argv[1]) as res:
-    print(res.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        print(res.headers.get('X-Request-Id'))
