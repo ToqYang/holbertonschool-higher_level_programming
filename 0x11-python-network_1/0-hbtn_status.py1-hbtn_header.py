3ded1cb1967ce063f388a326c9acfb7022c2d1d6
Santiago TY <santitoya-2001@outlook.com>
2020-01-13 17:02:27 -0500

    [Feature] Status

--- 0x11-python-network_1/0-hbtn_status.py1-hbtn_header.py
+++ 0x11-python-network_1/0-hbtn_status.py1-hbtn_header.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 """ What's my status? """
+import urllib.request
+
+
+req = urllib.request.Request('https://intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as res:
+    con = res.read()
+    enc = con.decode('utf-8')
+    namclas = type(con)
+    
+    print("Body response:")
+    print("\t- type: {}".format(namclas))
+    print("\t- content: {}".format(con))
+    print("\t- utf8 content: {}".format(enc))
+      
