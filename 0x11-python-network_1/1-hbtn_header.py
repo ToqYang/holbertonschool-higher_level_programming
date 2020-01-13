fad15de315e6022e75002bd9916952828d70ac88
Santiago TY <santitoya-2001@outlook.com>
2020-01-13 17:58:23 -0500

    [Feature] Header num

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 """ Response header value """
+import urllib.request
+import sys
+
+urlhol = sys.argv[1]
+
+req = urllib.request.Request(urlhol)
+with urllib.request.urlopen(req) as res:
+    print(res.getheader('X-Request-Id'))
