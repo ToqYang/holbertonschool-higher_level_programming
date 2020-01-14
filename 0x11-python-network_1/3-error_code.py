a1142ca165128d4a70c902cf1e2221fb6e90c808
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-14 22:15:38 +0000

    [Feature] Error code urllib

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 """ What's my status? """
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+
+    request = urllib.request.Request(sys.argv[1])
+
+    try:
+        with urllib.request.urlopen(request) as ans:
+            print(ans.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
