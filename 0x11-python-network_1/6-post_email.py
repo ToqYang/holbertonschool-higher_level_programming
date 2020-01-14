b10a3821fde4f222e12286ab14685cf6c274e712
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-14 23:05:08 +0000

    [Feature] Post email with requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 """ POST an email #1 """
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    data = {}
+    data['email'] = sys.argv[2]
+
+    req = request.post(sys.argv[1], data)
+    print(req)
