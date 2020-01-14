6ff1d485cca1bd5fe4defbe90f6f2b70bc5b900d
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-14 21:57:44 +0000

    [Feature] POST e-mail

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 """ Error code """
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {}
+    values['email'] = sys.argv[2]
+    
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read().decode('utf-8')
+        print(the_page)
