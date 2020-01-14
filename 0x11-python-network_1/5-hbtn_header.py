763e67ca556e78b9955ac45a125935b4a2eca618
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-14 22:45:03 +0000

    [Feature] Request headers

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 """ Response header value #1 """
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    req = requests.get(sys.argv[1])
+    print(req.headers['X-Request-Id'])
