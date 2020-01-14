a4dcb28a9f760a3f6559c43be972da64d672c16f
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-14 23:21:25 +0000

    [Feature] Error Status Code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 """ Error code #1 """
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    try:
+        req = requests.get(sys.argv[1])
+        print(req.text)
+    except:
+        print("Error code: {}".format(req.status_code))
