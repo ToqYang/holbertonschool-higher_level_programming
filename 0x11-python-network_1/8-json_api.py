149b3df33d20dbb78f1993b169ccb5688bf12627
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-15 20:44:13 +0000

    [Feature] Json advance

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 """ Search API """
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    req = requests.get('http://0.0.0.0:5000/search_user')
+
+    dat = {}
+    dat['q'] = sys.argv[1]
+
+    req = requests.post(sys.argv[1], data=dat)
+    print(req.text)
+
+    
