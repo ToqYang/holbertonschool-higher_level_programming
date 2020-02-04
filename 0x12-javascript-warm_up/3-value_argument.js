9e53f71baecb37825c405294b6cfe9098879839b
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-04 19:48:16 +0000

    [Fix] Add in case of this has not argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,8 +3,17 @@
    Print the value passed
    of the first argument
 */
+let idx = 0;
+let value = 'Holberton';
 process.argv.forEach((val, index) => {
   if (index === 2) {
-    console.log(val);
+    value = val;
   }
+  ++idx;
 });
+
+if (idx === 3) {
+  console.log(value);
+} else {
+  console.log('No argument');
+}
