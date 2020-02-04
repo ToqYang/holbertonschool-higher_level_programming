99c33a70123fd16ed76754e6a02fd8e8aea35a89
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-04 19:23:31 +0000

    [Fix] Message of == 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -7,7 +7,7 @@ myArgv = process.argv.length;
 if (myArgv <= 2) {
   console.log('No argument');
 } else if (myArgv === 3) {
-  console.log('Arguments found');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
