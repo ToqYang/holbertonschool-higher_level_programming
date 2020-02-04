df69462c0bc1d4cd405112365af5811aad21c756
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-04 20:30:24 +0000

    [Fix] Change const to let

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,5 +2,5 @@
 /*
    Loops, arrays in javascript
 */
-const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 lang.forEach(text => console.log(text));
