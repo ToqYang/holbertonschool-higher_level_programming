ffc481f47bb03418affc4eaa04a438f8626ec3cd
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-04 19:16:32 +0000

    [Fix] Condition at least one argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ const myArgv = process.argv.length;
 
 if (myArgv <= 2) {
   console.log('No argument');
+} else if (myArgv === 3) {
+  console.log('Arguments found');
 } else {
   console.log('Arguments found');
 }
