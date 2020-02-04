b3aa641123841561873275b99f2a2b03290fd085
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-04 20:31:56 +0000

    [Fix] Other manner

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,5 +2,9 @@
 /*
    Loops, arrays in javascript
 */
-let lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-lang.forEach(text => console.log(text));
+const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let count = 0;
+while (count < lang.length) {
+  console.log(lang[count]);
+  ++count;
+}
