d6c5d91e0c26fe6ed13191991acf9470cfbb5870
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-04 23:35:45 +0000

    [Fix] Sumthe pass of the argument

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -3,8 +3,6 @@
   Incremet the value about each called
 */
 exports.addMeMaybe = function (x, theFunction) {
-  let i = 0;
-  for (i = 1; i <= x; ++i) {
-  }
-  theFunction(i);
+  x = x + 1;
+  theFunction(x);
 };
