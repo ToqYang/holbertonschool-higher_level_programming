816c8ededdb99578fe75f293d571ed9a75658bff
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-04 21:12:08 +0000

    [Feature] Print square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+   Print square about number
+*/
+let myArgc = 0;
+myArgc = process.argv.length;
+let num = 0;
+num = parseInt(process.argv[2]);
+
+if (myArgc <= 2 || isNaN(num)) {
+  console.log('Missing size');
+} else if (myArgc === 3) {
+  for (let i = 0; i < num; ++i) {
+    console.log('x'.repeat(num));
+  }
+}
