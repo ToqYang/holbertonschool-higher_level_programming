8cd98a60d7c4a736b797ab7e038cf5aa7ceeb31a
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-04 23:50:52 +0000

    [Feature] Adds function to object

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*
+  Increment a object
+*/
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  return this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
