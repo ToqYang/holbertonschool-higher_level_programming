a31e5a842e0e5cb4c751039e03310f15e9ab3336
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-10 22:58:39 +0000

    [Feature] Add Normal, Double and Rotate

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+/*
+ Defining Rectangle class
+ Defining two attributes width and height
+ If the number are negative or 0 the object is undefined
+ Add method print
+
+*/
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((h <= 0 || h === undefined) || (w <= 0 || w === undefined)) {
+
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; ++i) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    let tmp = 0;
+    tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+};
