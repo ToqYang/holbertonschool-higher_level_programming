8c649cd0415d9fe9d9fbd5d281b2d71a4175e9ee
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-10 20:28:25 +0000

    [Feature] Defines width and height

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Defining Rectangle class
+// Defining two attributes width and height
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
