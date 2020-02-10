3eef2e1e69a74c2453a2ad8abad9729fedb3e377
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-10 22:22:17 +0000

    [Feature] Add print method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/*
+ Defining Rectangle class
+ Defining two attributes width and height
+ If the number are negative or 0 the object is undefined
+
+*/
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((h <= 0 || h === undefined) || (w <= 0 || w === undefined)) {
+
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; ++i) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
