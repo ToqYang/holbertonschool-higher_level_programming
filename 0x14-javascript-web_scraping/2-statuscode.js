e61ca82310a5bf839d49ac58e53bbdc17f3e1a56
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-12 03:48:32 +0000

    [Feature] Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,3 +2,9 @@
 /*
   Display status code
 */
+const request = require('request');
+request
+  .get(process.argv[2])
+  .on('response', function (response) {
+    console.log('code: ' + response.statusCode);
+  });
