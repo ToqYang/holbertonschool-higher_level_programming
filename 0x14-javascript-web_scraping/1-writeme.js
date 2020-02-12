84c9666e27d734889d2ac745e243bf6b3dfb43f9
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-12 03:41:48 +0000

    [Feature] Write to file in utf-8

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,3 +2,13 @@
 /*
   Writes a string to a file.
 */
+const fs = require('fs');
+
+// Contain name of the file
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(process.argv[3]);
+  }
+});
