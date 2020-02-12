6f7b0ca69aac843f3959066a90a3a9e4e46d7648
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-12 04:10:37 +0000

    [Feature] Star was title base in the id

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,3 +3,14 @@
   Prints the title of a Star Wars
   based in the id
 */
+const request = require('request');
+let movtitle = 'http://swapi.co/api/films/';
+movtitle = movtitle.concat(process.argv[2]);
+
+request(movtitle, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
