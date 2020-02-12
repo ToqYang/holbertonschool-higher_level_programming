886bab6122e9ee3ba86cf9b8e9a86e34de7b3c20
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-12 04:39:54 +0000

    [Feature] Write content of the webpage

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,3 +3,20 @@
   Gets the contents of a webpage
   and stores it in a file
 */
+const request = require('request');
+const fs = require('fs');
+
+const urlName = process.argv[2];
+const nameFile = process.argv[3];
+
+request(urlName, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  // Contain name of the file
+  fs.writeFile(nameFile, body, 'utf-8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
