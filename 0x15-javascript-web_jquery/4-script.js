339e739af9d6de7939925be797a1a718c01e7988
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 12:53:00 -0500

    [Fix] Proves the toggle method

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -5,12 +5,6 @@
 */
 $(document).ready(function () {
   $('div#toggle_header').click(function () {
-    if ($('header'.hasClass('red'))) {
-      $('div#toggle_header').toggleClass('green');
-    } else if ($('header'.hasClass('green'))) {
-      $('div#toggle_header').toggleClass('red');
-    } else if ($('header').is(':empty')) {
-      $('div#toggle_header').toggleClass('red');
-    }
+    $('header').toggleClass('red');
   });
 });
