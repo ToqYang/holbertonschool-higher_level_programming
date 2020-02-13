08bd0630f7f0e9f05277e9597953f9a55f79a812
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 17:27:01 -0500

    [Fix] Try other manner

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,15 +1,12 @@
-#!/usr/bin/node
 /*
   fetches and lists all movies
   title by using this URL: https://swapi.co/api/films/?format=json
 */
 $(document).ready(function () {
-  $.getJSON('https://swapi.co/api/people/5/?format=json', function (result) {
-    $.each(result, function (i, field) {
-      if (i === 'title') {
-        const name = '<li>' + field + '<li>';
-        $('ul#list_movies').append(name);
-      }
-    });
+  $.getJSON('https://swapi.co/api/films/?format=json', function (result) {
+    for (const mov of result.results) {
+      let movie = "<li>" + mov.title + "</li>"
+      $('ul#list_movies').append(movie);
+    }
   });
 });
