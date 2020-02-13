f814476519a86197c5d622cacf352956cfc5a1e3
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 12:38:38 -0500

    [Feature] Toggle function

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -3,3 +3,9 @@
   Updates the text color
   to #FF0000 clicks change the color
 */
+$(document).ready(function () {
+  $('div#toggle_header').click(function () {
+    $('div#toggle_header').css('color', '#FF0000');
+    $('div#toggle_header').toggleClass('red');
+  });
+});
