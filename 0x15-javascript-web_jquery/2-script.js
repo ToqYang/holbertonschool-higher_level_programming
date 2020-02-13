988d74b4c648a0a1b7804602c467f2763dc4f009
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 11:59:59 -0500

    [Feature] On click change color

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -3,3 +3,8 @@
   Updates the text color
   to #FF0000 clicks on the tag DIV#red_header
 */
+$(document).ready(function () {
+  $('div#red_header').click(function () {
+    $('div#red_header').css('color', '#FF0000');
+  });
+});
