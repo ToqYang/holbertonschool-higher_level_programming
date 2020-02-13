b6fbebbee05772a1cb938aff6d179ace4e4b62ee
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 14:24:23 -0500

    [Feature] Add li to ul

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -2,3 +2,8 @@
 /*
   Write a Javascript script that adds a LI
 */
+$(document).ready(function () {
+  $('div#red_header').click(function () {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+});
