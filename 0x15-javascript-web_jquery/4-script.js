98b0217e65d3eb313ed264af2da86ae41ec60b2e
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 14:14:36 -0500

    [Fix] Tag to apply changes

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -6,11 +6,11 @@
 $(document).ready(function () {
   $('div#toggle_header').click(function () {
     if ($('header').hasClass('red')) {
-      $("#div1").removeClass("red");
-      $("#div1").addClass("green");
+      $("header").removeClass("red");
+      $("header").addClass("green");
     } else if ($('header').hasClass('green')) {
-      $("#div1").removeClass("green");
-      $("#div1").addClass("red");
+      $("header").removeClass("green");
+      $("header").addClass("red");
     } else if ($('header').is(':empty')) {
       $('header').toggleClass('red');
     }
