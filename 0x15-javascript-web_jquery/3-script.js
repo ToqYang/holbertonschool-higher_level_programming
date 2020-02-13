a769fbab3f62bf1a2fc37ebd66d6322a87e74cdf
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 12:11:31 -0500

    [Feature] Add class red

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -3,3 +3,9 @@
   Updates the text color
   to #FF0000 with .red class
 */
+$(document).ready(function () {
+  $('div#red_header').click(function () {
+    $('header').addClass('red');
+    $('div#red_header').css('color', '#FF0000');
+  });
+});
