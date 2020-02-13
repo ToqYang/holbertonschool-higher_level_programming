324cae962b24e12d7f60434c1ad9118a4f65a17c
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 16:44:57 -0500

    [Fix] Traversing a dictionary

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
 /*
-  fetches and lists all movies 
+  fetches and lists all movies
   title by using this URL: https://swapi.co/api/films/?format=json
 */
 $(document).ready(function () {
   $.getJSON('https://swapi.co/api/people/5/?format=json', function (result) {
-      let name = "<li>" + result.name + "<li>" 
-      $('ul#list_movies').append(name);
+    $.each(result, function (i, field) {
+      if (i === 'title') {
+        const name = '<li>' + field + '<li>';
+        $('ul#list_movies').append(name);
+      }
     });
   });
+});
