946c35fbc50221a1e13c3c852eaf0251c34aa765
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 15:05:58 -0500

    [Feature] Star wars character

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
 /*
-  Fetches and replaces the 
+  Fetches and replaces the
   name of this URL: https://swapi.co/api/people/5/?format=json
 */
+$(document).ready(function () {
+  $('button').click(function () {
+    $.getJSON('https://swapi.co/api/people/5/?format=json', function (result) {
+      $.each(result, function (key, value) {
+        if (key === 'name') {
+          $('div#character').append(value + ' ');
+        }
+      });
+    });
+  });
+});
