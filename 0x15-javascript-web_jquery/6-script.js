4dbf73aba2bcb8e51ec8e3a93ea3afcd94cb0bf4
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 14:42:14 -0500

    [Feature] Add the new text

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -2,3 +2,8 @@
 /*
   Updates the text of the HTML tag HEADER to “New Header!!!”
 */
+$(document).ready(function () {
+  $('div#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
+});
