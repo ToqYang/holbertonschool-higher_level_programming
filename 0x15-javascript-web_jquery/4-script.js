74febec9d9e3d72af1cc033833d0dde08a52ff2c
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 12:40:58 -0500

    [Fix] Delete color

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -5,7 +5,6 @@
 */
 $(document).ready(function () {
   $('div#toggle_header').click(function () {
-    $('div#toggle_header').css('color', '#FF0000');
     $('div#toggle_header').toggleClass('red');
   });
 });
