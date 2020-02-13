64384f2f105b86f16d09c1d862ad2fb081f0c5f9
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 17:42:47 -0500

    [Feature] Say hello in french

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -4,3 +4,8 @@
   https://fourtonfish.com/hellosalut/?lang=fr and 
   displays the value of hello from that fetch in the HTML’s tag DIV#hello.
 */
+$(document).ready(function () {
+  $.getJSON('https://fourtonfish.com/hellosalut/?lang=fr', function (result) {
+      $('div#hello').text(result.hello);
+    });
+});
