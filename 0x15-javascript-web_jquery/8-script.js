2aabbf8463bb7e225efc4f785888a3c3fbe2b805
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 16:37:50 -0500

    [Feature] Add movie

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -3,3 +3,9 @@
   fetches and lists all movies 
   title by using this URL: https://swapi.co/api/films/?format=json
 */
+$(document).ready(function () {
+  $.getJSON('https://swapi.co/api/people/5/?format=json', function (result) {
+      let name = "<li>" + result.name + "<li>" 
+      $('ul#list_movies').append(name);
+    });
+  });
