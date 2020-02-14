3e8c2ef69a615f8e1a9311edf51a556e3c752c9e
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 23:38:29 -0500

    [Fix] Add when press enter

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -4,10 +4,20 @@
 */
 document.addEventListener('DOMContentLoaded', function () {
   $('input#btn_translate').click(function () {
-    const code = $('input#language_code').val();
-    url = 'https://fourtonfish.com/hellosalut/?lang=' + code;
+    let code = $('input#language_code').val();
+    let url = 'https://fourtonfish.com/hellosalut/?lang=' + code;
     $.getJSON(url, function (result) {
       $('div#hello').text(result.hello);
     });
+  });
+
+  $(document).keypress(function (e) {
+    if (e.which == 13) {
+      let code = $('input#language_code').val();
+      let url = 'https://fourtonfish.com/hellosalut/?lang=' + code;
+      $.getJSON(url, function (result) {
+        $('div#hello').text(result.hello);
+      });
+    }
   });
 });
