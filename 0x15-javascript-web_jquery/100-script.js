00dde1cc455ade087b7ef796b5589fb21494c320
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-13 22:33:27 -0500

    [Feature] Charged of DOM before the changes

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+/*
+  Verify if the DOM is charged
+  and after change the color
+*/
+document.addEventListener('DOMContentLoaded', function () {
+  let element = document.querySelector('header');
+  element.style.color = '#FF0000';
+});
